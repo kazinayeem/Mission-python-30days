9c8ffbdb4b631401614022e3c780c1041d14e826
Mohammad Ali Nayeem <85398213+kazinayeem@users.noreply.github.com>
2024-04-03 00:53:57 +0600

    Update day-9.py

--- day-9.py
+++ day-9.py
@@ -1,98 +1,26 @@
-# Day 8-9: Tuples and dictionaries: properties, use cases.
-# name : kazi Nayeem
-# date : 1/04/2004
-# day 8
+# day 9
 
-mytuple = ("apple", "banana", "cherry")
-
-print(mytuple[0])
-print(mytuple[0:1])
-print(mytuple)
-
-print(type(mytuple))
-
-# duplicate tuple
+# Python Dictionaries
 
 
-thistuple = ("apple", "banana", "cherry", "apple", "cherry")
+thisdict = {
+    "brand": "Ford",
+    "model": "Mustang",
+    "year": 1964,
+    "colors": ["red", "white", "blue"]
+}
 
-if "apple" in thistuple:
-    print("apple is in thistuple")
+thisdict["model"] = "newmodel"
+print(thisdict)
+print(type(thisdict))
+print(thisdict["year"])
+print(thisdict["brand"])
+print(thisdict["colors"][0])
 
-print(thistuple)
+print(len(thisdict))
 
-# check length
-print(len(thistuple))
+thisdict1 = dict(name="John", age=36, country="Norway")
+print(thisdict1)
 
-tuple1 = ("apple", "banana", "cherry")
-tuple2 = (1, 5, 7, 9, 3)
-tuple3 = (True, False, False)
-
-tuple1_mix = ("abc", 34, True, 40, "male")
-
-# update tuple
-
-
-x = ("apple", "banana", "cherry")
-
-y = list(x)
-
-y[1] = "kiwi"
-
-x = tuple(y)
-
-print(x)
-
-z = ("apple", "banana", "tomato", "orange")
-
-# convert list then change or add anything's
-
-
-list_z = list(z)
-
-list_z.append("malta")
-# remove
-
-
-list_z.remove("tomato")
-print(list_z)
-# also we can use del method
-print(type(list_z))
-
-tuple_z = tuple(list_z)
-
-print(tuple_z)
-
-# loop in tuple
-
-
-new_tuple = ("apple", "banana", "tomato", "malta", "mango")
-
-print(new_tuple)
-
-for i in new_tuple:
-    print(i)
-
-# loop use range
-print("____________________ for  loop _______________________________")
-
-for i in range(len(new_tuple)):
-    print(new_tuple[i])
-
-# while loop
-print("_________________________while loop __________________________")
-
-i = 0
-
-while i < len(new_tuple):
-    print(new_tuple[i])
-    i = i + 1
-
-data = list()
-data_len = int(input("enter your data len ::  "))
-
-for i in range(data_len):
-    d = input(f"Enter your data  {i + 1}  :  ")
-    data.append(d)
-
-print(data)
+for x in thisdict1.values():
+    print(x)
