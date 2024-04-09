4715b1f4b212a22fdcc2ae6c68c956977d11a7e5
Mohammad Ali Nayeem <85398213+kazinayeem@users.noreply.github.com>
2024-04-09 11:39:33 +0600

    day 16

---
+++ day-16.py
@@ -0,0 +1,14 @@
+# import
+# day 16
+
+import os
+import time
+import random
+print(os.getcwd())
+print(os.listdir())
+print(random.randint(1,100))
+i = 0
+while True:
+    time.sleep(4)
+    print("HI", i)
+    i += 1
